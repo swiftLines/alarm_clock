ccfdac644a48801fb52ef51d5606f9fca7c49cb8
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-07 18:17:31 -0500

    fix format of clock and return after each clear

--- alarm_clock.py
+++ alarm_clock.py
@@ -2,7 +2,7 @@ from playsound import playsound
 import time
 
 CLEAR = '\033[2J'
-CLEAR_AND_RETURN = '\033'
+CLEAR_AND_RETURN = '\033[H'
 
 def alarm(seconds):
   time_elapsed = 0
@@ -16,7 +16,7 @@ def alarm(seconds):
     minutes_left = time_left // 60
     seconds_left = time_left % 60
     
-    print(f'{CLEAR_AND_RETURN}Alarm will sound in:{minutes_left:02d}:{seconds_left:02d}')
+    print(f'{CLEAR_AND_RETURN} Alarm will sound in: {minutes_left:02d}:{seconds_left:02d}')
 
   playsound('alarm.mp3')
 
